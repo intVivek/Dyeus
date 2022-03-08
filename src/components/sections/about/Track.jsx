87e838ff0e4f0c15693e18833938f0019dd6e86e
Vivek Srivastava <viveksrivastava6180@gmail.com>
2022-03-09 04:50:46 +0530

    Add Track Section

---
+++ src/components/sections/about/Track.jsx
@@ -0,0 +1,26 @@
+import Woman1 from '../../../assets/img/picture/woman1.png';
+import Woman2 from '../../../assets/img/picture/woman2.png';
+import Woman3 from '../../../assets/img/picture/woman3.png';
+import Woman4 from '../../../assets/img/picture/woman4.png';
+import styles from '../../../assets/styles/components/sections/about/track.module.scss';
+import Button from '../../base/Button';
+
+export default function Track () {
+	return (
+		<div className={styles.track}>
+			<div className={styles.hero}>
+				<h1>Track your mood</h1>
+				<p>All days and all moods are not made equal. But we can only improve what we measure.
+					Track your mood and vitals (including skin and hair health)
+					so you can take better care of yourself every day.</p>
+				<Button className={styles.trackNow} label='Track you mood on the app' />
+			</div>
+			<div className={styles.footer}>
+				<img src={Woman1} />
+				<img src={Woman2} />
+				<img src={Woman3} />
+				<img src={Woman4} />
+			</div>
+		</div>
+	);
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import AppBar from '../components/panels/AppBar';
 import Flaunt from '../components/sections/about/Flaunt';
 import Landing from '../components/sections/about/Landing';
 import Promise from '../components/sections/about/Promise';
+import Track from '../components/sections/about/Track';
 
 export default function About () {
 	return (
@@ -11,6 +12,7 @@ export default function About () {
 			<Landing />
 			<Flaunt />
 			<Promise />
+			<Track />
 		</div>
 	);
 }
