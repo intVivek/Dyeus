a53ee403eb3bab84fb1db7dd05bf40e8e98f7962
Vivek Srivastava <viveksrivastava6180@gmail.com>
2022-03-08 23:28:04 +0530

    Add Eslint Support

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './assets/styles/index.scss';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
